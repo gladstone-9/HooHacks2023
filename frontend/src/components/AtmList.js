4c4c3f7c5b8f5cda3095e44cf8139cb5a5b99875
Nick Garrone <nkj9hk@virginia.edu>
2023-03-25 17:30:03 -0400

    work

--- frontend/src/components/AtmList.js
+++ frontend/src/components/AtmList.js
@@ -1,10 +1,14 @@
 import { Box } from "@mui/material";
+import { useEffect } from "react";
 
 let AtmList = (props) =>{
     let data = props.data;
-    console.log(data)
 
-    return (data["data"] ? <Box sx={{backgroundColor:"#E2E2E2", padding:"16px"}}>
+    useEffect(()=>{
+        console.log("DATA" + props.data);
+    }, [])
+
+    return (data["name"] ? <Box sx={{backgroundColor:"#E2E2E2", padding:"16px"}}>
         <h3>Your ATM</h3>
         <p>{data["name"] + " at " + data["address"]["street_number"] + " " + data["address"]["street_name"] + ", " + data["address"]["city"] + ", " + data["address"]["state"] + ", " + data["address"]["zip"]}</p>
     </Box> : <p>No ATM found</p>)
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -6,12 +6,14 @@ let Header = (props) =>{
     const [latitude, setLatitude] = useState("");
     const [longitude, setLongitude] = useState("");
     const [data, setData] = useState([]);
+    const [hasClicked, setHasClicked] = useState(false);
 
     let sendRequest = () =>{
         let url = 'http://api.nessieisreal.com/atms?lat='+ latitude + '&lng=' + longitude + '&rad='+ 100 + '&key=' + "";
+        setHasClicked(true);
         fetch(url)
         .then((response) => response.json())
-        .then((theData) => setData(theData["data"][0]));
+        .then((theData) => setData(theData["data"].length > 0 ? theData["data"][0] : []));
     }
 
     return <>
@@ -23,7 +25,7 @@ let Header = (props) =>{
             </Box>
             <Button onClick={sendRequest} variant="contained" sx={{maxWidth:"300px", marginTop:"16px", marginBottom:"32px"}}>Find ATM</Button>
             <Box>
-                {data.length ? <img src="Pig.png" height={500} sx={{marginTop:"32px"}}/> : <AtmList data={data} />}
+                {data.length === 0 ? hasClicked ? <p>No atms near you</p> : <img src="Pig.png" height={500} sx={{marginTop:"32px"}}/> : <AtmList data={data} />}
             </Box>
         </Box>
         
