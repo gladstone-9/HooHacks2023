5e6ed795363d598b487b08ac27ecb284ad40fa93
Nick Garrone <nkj9hk@virginia.edu>
2023-03-25 18:23:02 -0400

    api

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,6 +1,7 @@
 import {Button, Box, TextField} from "@mui/material";
 import {useState} from "react";
 import AtmList from "./AtmList";
+import LatLongLink from "./LatLongLink";
 
 let Header = (props) =>{
     const [latitude, setLatitude] = useState("");
@@ -8,8 +9,10 @@ let Header = (props) =>{
     const [data, setData] = useState([]);
     const [hasClicked, setHasClicked] = useState(false);
 
+    let apiKey = "";
+
     let sendRequest = () =>{
-        let url = 'http://api.nessieisreal.com/atms?lat='+ latitude + '&lng=' + longitude + '&rad='+ 100 + '&key=' + "";
+        let url = 'http://api.nessieisreal.com/atms?lat='+ latitude + '&lng=' + longitude + '&rad='+ 100 + '&key=' + apiKey;
         setHasClicked(true);
         fetch(url)
         .then((response) => response.json())
@@ -19,13 +22,15 @@ let Header = (props) =>{
     return <>
         <Box sx={{flexDirection: "column", display: "flex", alignItems: "center"}}>
             <h1>ATM Finder</h1>
+            <LatLongLink />
+            <br/>
             <Box>
                 <TextField onChange={(e)=> setLatitude(e.target.value)} id="Latitude" label="Latitude" variant="outlined" sx={{paddingRight: "3px"}}/>
                 <TextField onChange={(e)=> setLongitude(e.target.value)} id="Longitude" label="Longitude" variant="outlined" />
             </Box>
             <Button onClick={sendRequest} variant="contained" sx={{maxWidth:"300px", marginTop:"16px", marginBottom:"32px"}}>Find ATM</Button>
             <Box>
-                {data.length === 0 ? hasClicked ? <p>No atms near you</p> : <img src="Pig.png" height={500} sx={{marginTop:"32px"}}/> : <AtmList data={data} />}
+                {data.length === 0 ? hasClicked ? <Box><img src="https://www.clipartmax.com/png/full/330-3308894_clipart-document-not-found-clip-art.png" height={300}/><p>No atms near you</p></Box> : <img src="Pig.png" height={500} sx={{marginTop:"32px"}}/> : <AtmList data={data} />}
             </Box>
         </Box>
         
---
+++ frontend/src/components/LatLongLink.js
@@ -0,0 +1,6 @@
+let LatLongLink = (props) =>{
+    return <><p>Don't know you latitude or longitude? Find out </p> 
+    <a href="https://www.findlatitudeandlongitude.com/" >here</a></>
+}
+
+export default LatLongLink;
